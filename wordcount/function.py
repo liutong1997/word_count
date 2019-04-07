c01d5a3a41e4961371d39650cdb3cde9b8aaa01c
liutong1997 <707921012@qq.com>
2019-04-07 15:13:23 +0800

    修改了html文件

--- wordcount/function.py
+++ wordcount/function.py
@@ -9,5 +9,14 @@ def home(request):
 def count(request):
     len_count=len(request.GET['text'])
     usercount=request.GET['text']
-    return render(request,'count.html',{'len_count':len_count,'usercount':usercount})
+    word_count={}
+    for word in usercount:
+        if word not in word_count:
+            word_count[word] = word_count.get(word,0)+1
+        else:
+            word_count[word] = word_count.get(word,0)+1
+    sor_d = sorted(word_count.items(),key=lambda x:x[1],reverse=True)
+    return render(request,'count.html',
+                  {'len_count':len_count,'usercount':usercount,
+                   'wordcount':word_count,'sorted':sor_d})
 
